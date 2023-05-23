2b7e1a4fbc06cd52ca1e25c9840079d1911895a0
JenHLee <hj.jennie.lee@gmail.com>
2023-05-22 22:38:21 -0600

    create new project portfolio 2023

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,30 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Jungsu Abraham Sohn",
+    job: "Linux Software Developer | Siemens | Calgary, AB",
+    description:
+      "I am sure Jennie has the potential to be an exceptional developer due to her strong sense of responsibility, clear communication skills, proactive learning approach, and problem-solving abilities. Her dedication to continuous improvement and strong work ethic further enhance her capabilities.",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Kelvin Lee",
+    job: "General Manager | KOTRA Toronto | Toronto, ON",
+    description:
+      "Throughout Jennie's tenure with KOTRA Toronto, She excelled in diverse areas. She played a key role in designing advertising materials for the '2018 K-Beauty in Montreal' event, introducing Korean cosmetic companies to the Canadian market. With strong organizational skills, she successfully coordinated the trade delegation and provided valuable guidance and standards for Korean companies entering Canada. Jennie showcased technical expertise in media design, including video, graphics, and adept management of social media and websites.",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Kate Jung",
+    job: "Entrepreneurship Coordinator | City of Vaughan | Vaughan, ON",
+    description:
+      "Throughout my work with Jennie, I was truly impressed by her remarkable ability to consistently deliver work on time. She actively proposed constructive suggestions to enhance efficiency, showcasing her strategic mindset. Jennie's role required proficiency in international trade and fluency in both English and Korean, which proved crucial to the project's success. Importantly, she exhibited conscientiousness, intelligence, and dedication as an employee. I wholeheartedly trust in her future endeavors and enthusiastically recommend her for any support. If you require further information, I am more than willing to provide additional details.",
+  },
+];
