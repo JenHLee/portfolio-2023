9efb3974ba6c7fd23476a3ea88e9c8a3c5471bc4
JenHLee <hj.jennie.lee@gmail.com>
2023-06-26 16:28:30 -0600

    change the a tag in scrollup

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -2,17 +2,38 @@ import React from "react";
 import "./scrollup.css";
 
 const ScrollUp = () => {
+  const handleScrollUp = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   window.addEventListener("scroll", function () {
     const scrollUp = document.querySelector(".scrollup");
-    // when the scroll is higher than 560 viewport height, add the show-scroll class to a tag with the scroll-top class
-    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-    else scrollUp.classList.remove("show-scroll");
+    if (this.scrollY >= 560) {
+      scrollUp.classList.add("show-scroll");
+    } else {
+      scrollUp.classList.remove("show-scroll");
+    }
   });
+
   return (
-    <a href="#" className="scrollup">
+    <button onClick={handleScrollUp} className="scrollup">
       <i className="uil uil-arrow-up scrollup__icon"></i>
-    </a>
+    </button>
   );
 };
+
+// const ScrollUp = () => {
+//   window.addEventListener("scroll", function () {
+//     const scrollUp = document.querySelector(".scrollup");
+//     // when the scroll is higher than 560 viewport height, add the show-scroll class to a tag with the scroll-top class
+//     if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+//     else scrollUp.classList.remove("show-scroll");
+//   });
+//   return (
+//     <a href="#" className="scrollup">
+//       <i className="uil uil-arrow-up scrollup__icon"></i>
+//     </a>
+//   );
+// };
 
 export default ScrollUp;
